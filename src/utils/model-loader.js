9c0b7459634c4d8bc755ee9bb71e058b74daa88f
Mirosław Ciastek <mirek@swingdev.io>
2018-03-30 11:53:10 +0200

    Add spaceship model. Remove unused files

--- src/utils/model-loader.js
+++ src/utils/model-loader.js
@@ -1,29 +1,21 @@
 import * as THREE from 'three'
 import GLTF2Loader from 'three-gltf2-loader'
-import isEmpty from 'lodash/isEmpty'
-
-import { MODEL_NAMES } from 'root/config'
 
 GLTF2Loader(THREE)
 
-const handleLoad = (object, resolve, reject) => {
-  const models = {}
-
+const handleLoad = (object, resolve) => {
   object.scene.traverse((child) => {
     if (child.isMesh) {
-      Object.keys(MODEL_NAMES).forEach((key) => {
-        if (child.name.includes(MODEL_NAMES[key])) {
-          models[key] = child
-        }
-      })
+      console.log(child)
+      // Object.keys(MODEL_NAMES).forEach((key) => {
+      //   if (child.name.includes(MODEL_NAMES[key])) {
+      //     models[key] = child
+      //   }
+      // })
     }
   })
 
-  if (!isEmpty(models)) {
-    resolve(models)
-  } else {
-    reject(new Error('No models were found!'))
-  }
+  resolve(object.scene)
 }
 
 export default function (url) {
