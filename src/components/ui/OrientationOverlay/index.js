10b9dc4f7573d38eb5c596faff711cbaab90dba5
Mirosław Ciastek <mirek@swingdev.io>
2018-05-10 16:25:21 +0200

    Add orientation overlay component.

---
+++ src/components/ui/OrientationOverlay/index.js
@@ -0,0 +1,53 @@
+import { html, render } from 'lit-html'
+import classNames from 'classnames'
+
+import { BREAKPOINTS } from 'root/config'
+
+import './styles.scss'
+
+class OrientationOverlay {
+  constructor (container) {
+    this.container = container
+  }
+
+  init () {
+    this.handleOrientationChange()
+    window.addEventListener(
+      'orientationchange',
+      this.handleOrientationChange
+    )
+  }
+
+  handleOrientationChange = () => {
+    const screenSize = Math.max(window.screen.width, window.screen.height)
+    const screenOrientation = ('orientation' in window.screen)
+      ? window.screen.orientation.angle
+      : window.orientation
+
+    const isLandscape = Math.abs(screenOrientation) !== 90
+
+    const isSmallDevice = (screenSize <= BREAKPOINTS.large)
+    const isOverlayActive = isLandscape && isSmallDevice
+
+    this.render({ isActive: isOverlayActive })
+  }
+
+  render (props) {
+    const classes = classNames('orientation-overlay', {
+      'is-active': props.isActive
+    })
+
+    const template = html`
+      <div class="${classes}">
+        Please rotate your device to landscape mode
+      </div>
+    `
+
+    render(
+      template,
+      this.container
+    )
+  }
+}
+
+export default OrientationOverlay
